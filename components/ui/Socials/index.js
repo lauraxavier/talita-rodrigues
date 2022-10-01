ce0de7c560187337e9427ec7490c2ed12595dcee
lauraxavier <laura.ac.xavier@gmail.com>
2022-10-01 01:47:48 -0400

    target blank in socials links

--- components/ui/Socials/index.js
+++ components/ui/Socials/index.js
@@ -6,10 +6,10 @@ import { AiFillInstagram } from 'react-icons/ai';
 export default function Socials(props) {
     return (
         <div className={style.socialsGroup}>
-            <a href='https://www.facebook.com/profile.php?id=100062930839704' target="_blank">
+            <a href='https://www.facebook.com/profile.php?id=100062930839704' rel="noreferrer" target="_blank">
                 <FaFacebookF color="white" size="20px"/>
             </a>
-            <a href='https://www.instagram.com/tali.consultoria/' target="_blank">
+            <a href='https://www.instagram.com/tali.consultoria/' rel="noreferrer" target="_blank">
                 <AiFillInstagram color="white" size="20px" />
             </a>
         </div>
