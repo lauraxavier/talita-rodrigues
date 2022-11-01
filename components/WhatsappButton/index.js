2b55fb9c6752743efd743c680019f3e608686f5b
lauraxavier <laura.ac.xavier@gmail.com>
2022-11-01 16:26:42 -0400

    number whatsapp btn

--- components/WhatsappButton/index.js
+++ components/WhatsappButton/index.js
@@ -4,7 +4,7 @@ import { BsWhatsapp } from "react-icons/bs";
 
 export default function Whatsapp() {
     return (
-        <a href="https://web.whatsapp.com/send?phone=+5511958917345" rel="noreferrer" target="_blank" className={style.whatsappBtn}>
+        <a href="https://wa.me/5511958917345" rel="noreferrer" target="_blank" className={style.whatsappBtn}>
             <BsWhatsapp color="#3acf37" size="35px" />
         </a>
     )
