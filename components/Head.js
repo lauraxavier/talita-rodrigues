3551ec607df0efab9f3703f9264acf54a1d160ba
lauraxavier <laura.ac.xavier@gmail.com>
2022-11-01 10:41:43 -0400

    changes mobile height

--- components/Head.js
+++ components/Head.js
@@ -6,7 +6,6 @@ export default function HeadSite() {
       <title>Talita Rodrigues</title>
       <meta name="description" content="Especialista em consórcio, consórcio imobiliário, consórcio náutico, consórcio agro, consórcio aviação, o que é consórcio " />
       <link rel="icon" href="/favicon.png" />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     </Head>
   )
 }
